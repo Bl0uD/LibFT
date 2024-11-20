dd3f430fe2f473dd15f93c3a7bf547a033f5b7c9
Julien <ju@device-65.home>
2024-11-20 20:05:45 +0100

    auto update

--- ft_substr.c
+++ ft_substr.c
@@ -6,37 +6,38 @@
 /*   By: ju <ju@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 21:32:41 by jdupuis           #+#    #+#             */
-/*   Updated: 2024/11/20 19:58:59 by ju               ###   ########.fr       */
+/*   Updated: 2024/11/20 20:04:23 by ju               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*res;
-	size_t	i;
+	int		i;
 
 	if (!s)
 		return (NULL);
 	if (start >= ft_strlen(s))
 	{
-		res = ft_strnew(1);
+		res = (char *)malloc(sizeof(char));
+		res[0] = '\0';
 		return (res);
 	}
-	res = ft_strnew(len);
+	res = (char *)malloc(len + 1);
 	if (!res)
 		return (NULL);
 	i = 0;
-	while (i < len)
+	while (i < (int)len)
 	{
-		res[i] = s[start + (unsigned int)i];
+		res[i] = s[start + i];
 		i++;
 	}
 	res[i] = '\0';
 	return (res);
 }
-*/
+/*
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	int		i;
@@ -62,3 +63,4 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	str[i] = '\0';
 	return (str);
 }
+*/
