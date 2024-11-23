ebe317824697493c72235064160c12448cb5c397
Julien Dupuis <jdupuis@c3r2p7.42perpignan.fr>
2024-11-23 15:12:47 +0100

    auto update

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ju <ju@student.42.fr>                      +#+  +:+       +#+        */
+/*   By: jdupuis <jdupuis@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 23:19:12 by jdupuis           #+#    #+#             */
-/*   Updated: 2024/11/20 18:38:18 by ju               ###   ########.fr       */
+/*   Updated: 2024/11/23 15:12:05 by jdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	if (!lst || !f || !del)
 		return (NULL);
 	head = ft_lstnew(f(lst->content));
+	if (!head)
+		return (NULL);
 	n_lst = head;
 	while (lst && lst->next)
 	{
-		if (!n_lst)
-			return (NULL);
 		n_lst->next = ft_lstnew(f(lst->next->content));
+		if (!n_lst->next)
+		{
+			ft_lstclear(&head, del);
+			del(lst->next->content);
+			return (NULL);
+		}
 		n_lst = n_lst->next;
 		lst = lst->next;
 	}
